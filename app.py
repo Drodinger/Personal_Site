a5746b36c2af341f1a78eef366d7d928ddb4a14a
Drodinger <ri3yne@hotmail.com>
2023-11-17 18:20:04 +0000

    removed database stuff as it was casuing issues

--- app.py
+++ app.py
@@ -39,21 +39,3 @@ def contact():
 class Base(DeclarativeBase):
   pass
 
-db = SQLAlchemy(model_class=Base)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://flask:password@localhost:5432/personal_site_db"
-
-db.init_app(app)
-
-class Skills(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    backend: Mapped[str] = mapped_column(String, nullable=True)
-    frontend: Mapped[str] = mapped_column(String)
-    architectures: Mapped[str] = mapped_column(String)
-    methodologies: Mapped[str] = mapped_column(String)
-    databases: Mapped[str] = mapped_column(String)
-    devops: Mapped[str] = mapped_column(String)
-    other_technologies: Mapped[str] = mapped_column(String)
-    
-with app.app_context():
-    db.create_all()
